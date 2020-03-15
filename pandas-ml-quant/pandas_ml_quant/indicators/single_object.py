ddf9143d1e7e8425ebc550bb5b0bd09a627bdb54
kic <ch9.ki7@gmail.com>
2020-03-15 13:59:17 +0100

    add some naming to indicators

--- pandas-ml-quant/pandas_ml_quant/indicators/single_object.py
+++ pandas-ml-quant/pandas_ml_quant/indicators/single_object.py
@@ -5,7 +5,7 @@ import numpy as _np
 import pandas as _pd
 from scipy.stats import zscore
 
-from pandas_ml_quant.indicators.utils import wilders_smoothing
+from pandas_ml_quant.indicators.utils import wilders_smoothing as _ws, with_column_suffix as _wcs
 
 _PANDAS = _Union[_pd.DataFrame, _pd.Series]
 
@@ -15,25 +15,27 @@ def ta_inverse(df: _PANDAS) -> _PANDAS:
 
 
 def ta_sma(df: _PANDAS, period=12) -> _PANDAS:
-    return df.rolling(period).mean()
+    return _wcs(f"sma_{period}", df.rolling(period).mean())
 
 
 def ta_ema(df: _PANDAS, period=12) -> _PANDAS:
-    return df.ewm(span=period, adjust=False, min_periods=period-1).mean()
+    return _wcs(f"ema_{period}", df.ewm(span=period, adjust=False, min_periods=period-1).mean())
 
 
 def ta_wilders(df: _PANDAS, period=12) -> _PANDAS:
     if isinstance(df, _pd.Series):
-        return ta_wilders(df.to_frame(), period).iloc[:, 0]
+        res = ta_wilders(df.to_frame(), period).iloc[:, 0]
     else:
         resdf = _pd.DataFrame({}, index=df.index)
         for col in df.columns:
             s = df[col].dropna()
             res = _np.zeros(s.shape)
-            wilders_smoothing(s.values, period, res)
+            _ws(s.values, period, res)
             resdf = resdf.join(_pd.DataFrame({col: res}, index=s.index))
 
-        return resdf
+        res = resdf
+
+    return _wcs(f"wilders_{period}", res)
 
 
 def ta_macd(df: _PANDAS, fast_period=12, slow_period=26, signal_period=9, relative=True) -> _PANDAS:
@@ -42,8 +44,9 @@ def ta_macd(df: _PANDAS, fast_period=12, slow_period=26, signal_period=9, relati
     macd = (fast / slow - 1) if relative else (fast - slow)
     signal = ta_ema(macd, signal_period)
     hist = macd - signal
+    suffix = f'{fast_period},{slow_period},{signal_period}'
 
-    for label, frame in {"macd": macd, "signal": signal, "histogram": hist}.items():
+    for label, frame in {f"macd_{suffix}": macd, f"signal_{suffix}": signal, f"histogram_{suffix}": hist}.items():
         if isinstance(frame, _pd.DataFrame) and len(df.columns) > 1:
             frame.columns = _pd.MultiIndex.from_product([frame.columns, [label]])
         else:
@@ -54,15 +57,15 @@ def ta_macd(df: _PANDAS, fast_period=12, slow_period=26, signal_period=9, relati
 
 
 def ta_mom(df: _PANDAS, period=10) -> _PANDAS:
-    return df.diff(period)  # FIXME .add_suffix("_mom")
+    return _wcs(f"mom_{period}", df.diff(period))
 
 
 def ta_roc(df: _PANDAS, period=10) -> _PANDAS:
-    return df.pct_change(period)  # FIXME .add_suffix("_roc")
+    return _wcs(f"roc_{period}", df.pct_change(period))
 
 
 def ta_stddev(df: _PANDAS, period=5, nbdev=1, ddof=1) -> _PANDAS:
-    return (df.rolling(period).std(ddof=ddof) * nbdev)  # FIXME .add_suffix("_stddev")
+    return _wcs(f"stddev_{period}", (df.rolling(period).std(ddof=ddof) * nbdev))
 
 
 def ta_rsi(df: _PANDAS, period=14):
@@ -71,17 +74,95 @@ def ta_rsi(df: _PANDAS, period=14):
     pos = ta_wilders(returns.clip(lower=0), period)
     neg = ta_wilders(_np.abs(returns.clip(upper=0)), period)
 
-    return pos / (pos + neg)
+    return _wcs(f"rsi_{period}", pos / (pos + neg), returns)
 
 
 def ta_apo(df: _PANDAS, fast_period=12, slow_period=26, exponential=False) -> _PANDAS:
     fast = ta_ema(df, fast_period) if exponential else ta_sma(df, fast_period)
     slow = ta_ema(df, slow_period) if exponential else ta_sma(df, slow_period)
-    return fast - slow
+    return _wcs(f"apo_{fast_period},{slow_period},{int(exponential)}", fast - slow, df)
 
 
 def ta_trix(df: _PANDAS, period=30) -> _PANDAS:
-    return ta_ema(ta_ema(ta_ema(df, period), period), period).pct_change() * 100
+    return _wcs(f"trix_{period}", ta_ema(ta_ema(ta_ema(df, period), period), period).pct_change() * 100, df)
+
+
+def ta_ppo(df: _pd.DataFrame, fast_period=12, slow_period=26, exponential=True) -> _PANDAS:
+    fast = ta_ema(df, period=fast_period) if exponential else ta_sma(df, period=fast_period)
+    slow = ta_ema(df, period=slow_period) if exponential else ta_sma(df, period=slow_period)
+    return _wcs(f"ppo_{fast_period},{slow_period},{int(exponential)}", (fast - slow) / slow, df)
+
+
+def ta_zscore(df: _PANDAS, period=20, ddof=1):
+    return _wcs(f"z_{period}", df.rolling(period).apply(lambda c: zscore(c, ddof=ddof)[-1]))
+
+
+def ta_week_day(po: _PANDAS):
+    if not isinstance(po.index, _pd.DatetimeIndex):
+        df = po.copy()
+        df.index = _pd.to_datetime(df.index)
+    else:
+        df = po
+
+    return (df.index.dayofweek / 6.0).to_series(index=po.index, name="dow")
+
+
+def ta_week(po: _PANDAS):
+    if not isinstance(po.index, _pd.DatetimeIndex):
+        df = po.copy()
+        df.index = _pd.to_datetime(df.index)
+    else:
+        df = po
+
+    return (df.index.week / 52.0).to_series(index=po.index, name="week")
+
+
+def ta_ewma_covariance(df: _PANDAS, convert_to='returns', alpha=0.97):
+    data = df
+
+    if convert_to == 'returns':
+        data = df.pct_change()
+    if convert_to == 'log-returns':
+        data = _np.log(df) - _np.log(df.shift(1))
+
+    return data.ewm(com=alpha).cov()
+
+
+def ta_up_down_volatility_ratio(df: _PANDAS, period=60, normalize=True, setof_date=False):
+    if isinstance(df, _pd.DataFrame):
+        return df.apply(lambda col: ta_up_down_volatility_ratio(col, period, normalize))
+
+    returns = df.pct_change() if normalize else df
+    std = _pd.DataFrame({
+        "std+": returns[returns > 0].rolling(period).std(),
+        "std-": returns[returns < 0].rolling(period).std()
+    }, index=returns.index).fillna(method="ffill")
+
+    ratio = (std["std+"] / std["std-"] - 1).rename("std +/-")
+
+    # eventually we can off set the date such that we can fake one continuous data frame
+    if setof_date:
+        # +7 -1 binds us approximately to the same week day
+        ratio.index = ratio.index - _pd.DateOffset(days=(ratio.index[-1] - ratio.index[0]).days + 7 - 1)
+
+    return ratio
+
+
+def ta_multi_bbands(s: _pd.Series, period=5, stddevs=[0.5, 1.0, 1.5, 2.0], ddof=1) -> _PANDAS:
+    assert isinstance(s, _pd.Series)
+    mean = s.rolling(period).mean().rename("mean")
+    std = s.rolling(period).std(ddof=ddof)
+    df = _pd.DataFrame({}, index=mean.index)
+
+    for stddev in reversed(stddevs):
+        df[f'lower-{stddev}'] = mean - (std * stddev)
+
+    df["mean"] = mean
+
+    for stddev in stddevs:
+        df[f'upper-{stddev}'] = mean + (std * stddev)
+
+    return df
 
 
 def ta_bbands(df: _PANDAS, period=5, stddev=2.0, ddof=1) -> _PANDAS:
@@ -113,81 +194,3 @@ def ta_bbands(df: _PANDAS, period=5, stddev=2.0, ddof=1) -> _PANDAS:
         .join(z_score) \
         .join(quantile) \
         .sort_index(axis=1)
-
-
-def ta_ppo(df: _pd.DataFrame, fast_period=12, slow_period=26, exponential=True) -> _PANDAS:
-    fast = ta_ema(df, period=fast_period) if exponential else ta_sma(df, period=fast_period)
-    slow = ta_ema(df, period=slow_period) if exponential else ta_sma(df, period=slow_period)
-    return (fast - slow) / slow
-
-
-def ta_up_down_volatility_ratio(df: _PANDAS, period=60, normalize=True, setof_date=False):
-    if isinstance(df, _pd.DataFrame):
-        return df.apply(lambda col: ta_up_down_volatility_ratio(col, period, normalize))
-
-    returns = df.pct_change() if normalize else df
-    std = _pd.DataFrame({
-        "std+": returns[returns > 0].rolling(period).std(),
-        "std-": returns[returns < 0].rolling(period).std()
-    }, index=returns.index).fillna(method="ffill")
-
-    ratio = (std["std+"] / std["std-"] - 1).rename("std +/-")
-
-    # eventually we can off set the date such that we can fake one continuous data frame
-    if setof_date:
-        # +7 -1 binds us approximately to the same week day
-        ratio.index = ratio.index - _pd.DateOffset(days=(ratio.index[-1] - ratio.index[0]).days + 7 - 1)
-
-    return ratio
-
-
-def ta_zscore(df: _PANDAS, period=20, ddof=1):
-    return df.rolling(period).apply(lambda c: zscore(c, ddof=ddof)[-1])
-
-
-def ta_ewma_covariance(df: _PANDAS, convert_to='returns', alpha=0.97):
-    data = df
-
-    if convert_to == 'returns':
-        data = df.pct_change()
-    if convert_to == 'log-returns':
-        data = _np.log(df) - _np.log(df.shift(1))
-
-    return data.ewm(com=alpha).cov()
-
-
-def ta_multi_bbands(s: _pd.Series, period=5, stddevs=[0.5, 1.0, 1.5, 2.0], ddof=1) -> _PANDAS:
-    assert isinstance(s, _pd.Series)
-    mean = s.rolling(period).mean().rename("mean")
-    std = s.rolling(period).std(ddof=ddof)
-    df = _pd.DataFrame({}, index=mean.index)
-
-    for stddev in reversed(stddevs):
-        df[f'lower-{stddev}'] = mean - (std * stddev)
-
-    df["mean"] = mean
-
-    for stddev in stddevs:
-        df[f'upper-{stddev}'] = mean + (std * stddev)
-
-    return df
-
-
-def ta_week_day(po: _PANDAS):
-    if not isinstance(po.index, _pd.DatetimeIndex):
-        df = po.copy()
-        df.index = _pd.to_datetime(df.index)
-    else:
-        df = po
-
-    return (df.index.dayofweek / 6.0).to_series(index=po.index, name="dow")
-
-
-def ta_week(po: _PANDAS):
-    if not isinstance(po.index, _pd.DatetimeIndex):
-        df = po.copy()
-        df.index = _pd.to_datetime(df.index)
-    else:
-        df = po
-
-    return (df.index.week / 52.0).to_series(index=po.index, name="week")
--- pandas-ml-quant/pandas_ml_quant/indicators/utils.py
+++ pandas-ml-quant/pandas_ml_quant/indicators/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from numba import guvectorize, float32, int32, float64, int64
 
 
@@ -14,3 +15,20 @@ def wilders_smoothing(arr: np.ndarray, period: int, res: np.ndarray):
     for i in range(period, len(arr)):
         res[i] = alpha * res[i-1] + arr[i] * beta
 
+
+def with_column_suffix(suffix, po, ref_po=None):
+    if ref_po is None:
+        ref_po = po
+
+    if isinstance(po, pd.Series):
+        if isinstance(po.name, tuple):
+            return po.rename((suffix, *ref_po.name))
+        else:
+            return po.rename(f'{ref_po.name}_{suffix}')
+    else:
+        if isinstance(po.index, pd.MultiIndex):
+            po.columns = pd.MultiIndex.from_tuples([(suffix, *col) for col in ref_po.columns.to_list()])
+            return po
+        else:
+            po.columns = ref_po.columns
+            return po.add_suffix(f'_{suffix}')
