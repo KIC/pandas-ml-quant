9d6dba581ffd5c8fb98a064d1895f8a4c0a77ce7
kic <ch9.ki7@gmail.com>
2020-03-17 16:17:59 +0100

    cleanup

--- pandas-ml-utils/pandas_ml_utils/ml/data/reconstruction/prediction.py
+++ pandas-ml-utils/pandas_ml_utils/ml/data/reconstruction/prediction.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-# make sure to import mokey patched data frame
+# make sure to import monkey patched data frame
 from pandas_ml_common import pd, np, get_pandas_object
 from pandas_ml_utils.constants import PREDICTION_COLUMN_NAME, TARGET_COLUMN_NAME
 
@@ -51,56 +51,3 @@ def map_prediction_to_target(df, prediction, targets):
     return pd.DataFrame({"prediction": p.reshape((-1,))},
                         index=pd.MultiIndex.from_tuples(index))
 
-
-"""
-def prediction_to_frame(df,
-                        prediction: np.ndarray,
-                        index: pd.Index = None,
-                        inclusive_labels: bool = False,
-                        inclusive_source: bool = False) -> Union[pd.DataFrame, Dict[str, pd.DataFrame]]:
-    # sanity check
-    if not isinstance(prediction, np.ndarray):
-        raise ValueError(f"got unexpected prediction: {type(prediction)}\n{prediction}")
-
-    # assign index
-    index = self._df.index if index is None else index
-
-    # eventually fix the shape of the prediction
-    if len(prediction.shape) == 1:
-        prediction = prediction.reshape(len(prediction), 1)
-
-    # prediction_columns
-    columns = pd.MultiIndex.from_tuples(self.label_names(PREDICTION_COLUMN_NAME))
-    multi_dimension_prediction = len(prediction.shape) > 1 and len(columns) < prediction.shape[1]
-    if multi_dimension_prediction:
-        if len(prediction.shape) < 3:
-            df = pd.DataFrame({"a": [r.tolist() for r in prediction]}, index=index)
-        else:
-            df = pd.DataFrame({col: [row.tolist() for row in prediction[:, col]] for col in range(prediction.shape[1])},
-                              index=index)
-
-        df.columns = columns
-    else:
-        df = pd.DataFrame(prediction, index=index, columns=columns)
-
-    # add labels if requested
-    if inclusive_labels:
-        dfl = self.labels_df
-        dfl.columns = pd.MultiIndex.from_tuples(self.label_names(LABEL_COLUMN_NAME))
-        df = df.join(dfl, how='inner')
-
-        # add loss if provided
-        loss_df = self.gross_loss_df
-        df = df.join(loss_df.loc[df.index], how='inner') if loss_df is not None else df
-
-    # add target if provided
-    target_df = self.target_df
-    df = df.join(target_df.loc[df.index], how='inner') if target_df is not None else df
-
-    # also add source if requested
-    if inclusive_source:
-        df = df.join(self.source_df, how='inner')
-
-    # finally we can return our nice and shiny df
-    return df
-"""
