88d97f4ce4201196804d6b4c6a8489bc716529a1
kic <ch9.ki7@gmail.com>
2020-03-20 08:58:43 +0100

    improve deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,24 @@ SCRIPT="$(readlink --canonicalize-existing "$0")"
 SCRIPTPATH="$(dirname "$SCRIPT")"
 source "$SCRIPTPATH/.env.sh"
 
-python setup.py sdist
-twine upload dist/*
+deploy()
+{
+    echo "deploy: `pwd`"
+    python setup.py sdist
+    twine upload dist/*
+    cp -r dist /tmp/
+    rm -rf *.egg-info dist
+}
 
-cp -r dist /tmp/
-rm -rf *.egg-info dist
+if [ $# -eq 0 ]
+  then
+    deploy
+else
+  DIR=`pwd`
+  for var in "$@"
+    do
+      cd "$DIR/$var"
+      deploy
+    done
+fi
 
