7cbf7b5cdf3ea4f499f4877d0b1313e66677b7c2
kic <ch9.ki7@gmail.com>
2020-05-24 17:49:05 +0200

    enhance multi_model labeling

--- pandas-ml-utils/pandas_ml_utils_test/ml/data/model/test_sk_multi_model.py
+++ pandas-ml-utils/pandas_ml_utils_test/ml/data/model/test_sk_multi_model.py
@@ -26,7 +26,7 @@ class TestSkMultiModel(TestCase):
                 FeaturesAndLabels(features=["a", "b"], labels=["c", "d"], label_type=int),
             ),
             2,
-            True,
+            model_index_variable=None
         )
 
         fit = df.model.fit(model, NaiveSplitter(0.49), epochs=1500, verbose=True)
@@ -36,5 +36,33 @@ class TestSkMultiModel(TestCase):
         np.testing.assert_array_almost_equal(
             fit.training_summary.df[PREDICTION_COLUMN_NAME, "c"],
             fit.training_summary.df[PREDICTION_COLUMN_NAME, "d"]
+        )
+
+    def test_multi_model_kwargs(self):
+        """given some toy classification data"""
+        df = pd.DataFrame({
+            "a": [1, 0, 1, 0, 1, 0, 1, 0,],
+            "b": [0, 0, 1, 1, 0, 0, 1, 1,],
+            "c": [1, 0, 0, 1, 1, 0, 0, 1,],
+        })
+
+        model = MultiModel(
+            SkModel(
+                MLPClassifier(activation='logistic', max_iter=1000, hidden_layer_sizes=(3,), alpha=0.001,
+                              solver='lbfgs', random_state=42),
+                FeaturesAndLabels(features=["a", "b"], labels=[lambda df, index: df["c"].rename(index)], label_type=int),
+            ),
+            ["c1", "c2"],
+            model_index_variable="index"
+        )
+
+        fit = df.model.fit(model, NaiveSplitter(0.49), epochs=1500, verbose=True)
+        print(fit.training_summary.df)
+
+        self.assertEqual(4, len(fit.training_summary.df[PREDICTION_COLUMN_NAME, "c1"]))
+        self.assertEqual(4, len(fit.training_summary.df[PREDICTION_COLUMN_NAME, "c2"]))
+        np.testing.assert_array_almost_equal(
+            fit.training_summary.df[PREDICTION_COLUMN_NAME, "c1"],
+            fit.training_summary.df[PREDICTION_COLUMN_NAME, "c2"]
         )
 
