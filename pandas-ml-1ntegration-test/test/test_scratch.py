6ffbcddf11d60c14cee33e9649521a65b8e5a798
KIC <>
2020-07-13 14:30:22 +0200

    cleanup

---
+++ pandas-ml-1ntegration-test/test/test_scratch.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from sklearn.neural_network import MLPClassifier
+
+from pandas_ml_utils import SkModel
+
+
+class ScratchTests(TestCase):
+
+    def test_scratchl(self):
+        pass
