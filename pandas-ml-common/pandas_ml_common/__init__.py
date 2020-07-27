debb7ea47a3ec05e3eb2da0ad0a69eb895b8fee4
KIC <>
2020-07-27 08:54:06 +0200

    init 0.1.13

--- pandas-ml-common/pandas_ml_common/__init__.py
+++ pandas-ml-common/pandas_ml_common/__init__.py
@@ -1,5 +1,5 @@
 """Augment pandas DataFrame with methods for machine learning"""
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 
 import logging
 from typing import Union, List, Callable, Any
--- pandas-ml-quant-data-provider/setup.py
+++ pandas-ml-quant-data-provider/setup.py
@@ -1,5 +1,5 @@
 """Augment pandas DataFrame with methods to fetch time series data for quant finance"""
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 
 import os
 
