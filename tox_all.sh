0654e2c5feb5a6b8e7c5ae54fbab6ced099fee01
kic <kic@kic.kic>
2020-10-06 14:51:35 +0200

    add tox message

--- tox_all.sh
+++ tox_all.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 set -e
-ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # make sure we start with a clean environment
+echo "clean environment"
 rm -rf "$ABSOLUTE_PATH/.tox"
 
 # run all tox tests
-cd "$ABSOLUTE_PATH/pandas-ml-common" && tox && cd - || exit
-cd "$ABSOLUTE_PATH/pandas-ml-utils" && tox && cd - || exit
-cd "$ABSOLUTE_PATH/pandas-ml-utils-torch" && tox && cd - || exit
-# cd "$ABSOLUTE_PATH/pandas-ml-utils-keras" && tox && cd - || exit      # temporarily deprecate keras
-cd "$ABSOLUTE_PATH/pandas-ml-quant" && tox && cd - || exit
+echo "run tox tests"
+cd "$ABSOLUTE_PATH/pandas-ml-common" && tox && echo "done $(pwd)" && cd - || exit
+cd "$ABSOLUTE_PATH/pandas-ml-utils" && tox && echo "done $(pwd)" && cd - || exit
+cd "$ABSOLUTE_PATH/pandas-ml-utils-torch" && tox && echo "done $(pwd)" && cd - || exit
+# cd "$ABSOLUTE_PATH/pandas-ml-utils-keras" && tox && echo "done $(pwd)" && cd - || exit      # temporarily deprecate keras
+cd "$ABSOLUTE_PATH/pandas-ml-quant" && tox && echo "done $(pwd)" && cd - || exit
 # TODO finalize remaining tox tests: pandas-ml-quant-data-provider, pandas-ml-1ntegration-test
 
 # NOTE
