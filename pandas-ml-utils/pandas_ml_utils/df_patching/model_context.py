1bc51111904a2c58acba722d5f2799a9cc9ce1d8
kic <kic@kic.kic>
2020-10-15 09:24:19 +0200

    add model file name template to allow model versioning

--- pandas-ml-utils/pandas_ml_utils/df_patching/model_context.py
+++ pandas-ml-utils/pandas_ml_utils/df_patching/model_context.py
@@ -1,18 +1,22 @@
-from collections import namedtuple
+from datetime import datetime
+from string import Template
 from typing import Callable, Tuple, Dict, Union, List
 
 from pandas_ml_common import Typing
 from pandas_ml_common.sampling import naive_splitter
-from pandas_ml_utils.ml.model.base_model import Model
-from pandas_ml_utils.ml.data.extraction.features_and_labels_extractor import FeaturesWithLabels
+from pandas_ml_common.utils.time_utils import seconds_since_midnight
 from pandas_ml_utils.ml.data.extraction.features_and_labels_definition import FeaturesAndLabels
+from pandas_ml_utils.ml.data.extraction.features_and_labels_extractor import FeaturesWithLabels
+from pandas_ml_utils.ml.model.base_model import Model
 
 
 class ModelContext(object):
 
     def __init__(self, df: Typing.PatchedDataFrame, file_name: str = None):
         self.df = df
-        self.file_name = file_name
+        self.file_name = Template(file_name).substitute({
+            "V": f'{datetime.now().strftime("%Y%m-%d")}-{seconds_since_midnight()}'
+        }) if file_name is not None else None
 
     def fit(self,
             model: Callable[[], Model],
