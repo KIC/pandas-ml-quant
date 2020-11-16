c3484f6f4b517d8eea8c4d69e0023cbfa3a64cbd
kic <kic@kic.kic>
2020-11-16 19:20:38 +0100

    resolve todo in time indicators

--- pandas-ml-quant/pandas_ml_quant/technichal_analysis/indicators/time.py
+++ pandas-ml-quant/pandas_ml_quant/technichal_analysis/indicators/time.py
@@ -3,15 +3,16 @@ import pandas as _pd
 
 # create convenient type hint
 from pandas_ml_common import Typing as _t
+from pandas_ml_quant.technichal_analysis._decorators import *
 
 
-# TODO for_each_top_level_row
+@for_each_top_level_row
 def ta_decimal_year(df: _t.PatchedPandas):
     return ((df.index.strftime("%j").astype(float) - 1) / 366 + df.index.strftime("%Y").astype(float))\
         .to_series(index=df.index, name="decimal_time")
 
 
-# TODO for_each_top_level_row
+@for_each_top_level_row
 def ta_sinusoidal_week_day(po: _t.PatchedPandas):
     if not isinstance(po.index, _pd.DatetimeIndex):
         df = po.copy()
@@ -22,7 +23,7 @@ def ta_sinusoidal_week_day(po: _t.PatchedPandas):
     return _np.sin(2 * _np.pi * (df.index.dayofweek / 6.0)).to_series(index=po.index, name="dow")
 
 
-# TODO for_each_top_level_row
+@for_each_top_level_row
 def ta_sinusoidal_week(po: _t.PatchedPandas):
     if not isinstance(po.index, _pd.DatetimeIndex):
         df = po.copy()
