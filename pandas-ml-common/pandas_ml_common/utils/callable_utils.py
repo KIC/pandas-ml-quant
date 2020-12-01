20c4ff2009a99218d83297c84640c4e8cdf61bd5
kic <kic@kic.kic>
2020-12-01 18:50:11 +0100

    add log to call silent

--- pandas-ml-common/pandas_ml_common/utils/callable_utils.py
+++ pandas-ml-common/pandas_ml_common/utils/callable_utils.py
@@ -6,10 +6,12 @@ _log = logging.getLogger(__name__)
 _DEBUG = False
 
 
-def call_silent(func, default=None):
+def call_silent(func, default=None, log=False, log_ctx=None):
     try:
         return func()
     except Exception as e:
+        if log:
+            _log.warning(f"failed {func} call: {log_ctx}: {e}")
         return default(e) if callable(default) else default
 
 
