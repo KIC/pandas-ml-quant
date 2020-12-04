a53cbfb71308e2b0ebf279683c6fa0f1673be1e9
kic <kic@kic.kic>
2020-12-04 12:12:54 +0100

    post fix price adjustments for some rare cases

--- pandas-ml-quant-data-provider/pandas_ml_quant_data_provider/data_provider/yfinance_provider.py
+++ pandas-ml-quant-data-provider/pandas_ml_quant_data_provider/data_provider/yfinance_provider.py
@@ -3,6 +3,7 @@ import traceback
 
 import yfinance as yf
 from cachier import cachier
+from yfinance import utils as yf_utils
 
 from pandas_ml_common import pd
 from pandas_ml_quant_data_provider.data_provider.time_utils import time_until_end_of_day
@@ -39,12 +40,14 @@ def _download_yahoo_data(symbol, period, **kwargs):
 
         try:
             # first try to append the most recent data
-            return yf.Ticker(symbol).history(period="1d", interval='1d')[-1:].combine_first(df)
+            df = yf.Ticker(symbol).history(period="1d", interval='1d')[-1:].combine_first(df)
         except IOError:
             traceback.print_exc()
             logging.warning(
                 'failed to add yf.Ticker({v}).history(period="1d", interval="1d")[-1:] fallback to hist only!')
-            return df
+
+        # if 'Adj Close' in df then utils.auto_adjust(df)
+        return yf_utils.auto_adjust(df) if 'Adj Close' in df else df
 
 
 @cachier(stale_after=time_until_end_of_day())
--- pandas-ml-quant-data-provider/scraper/fetch_yfinance.py
+++ pandas-ml-quant-data-provider/scraper/fetch_yfinance.py
@@ -1,8 +1,12 @@
 import logging
 import os
+
 import pandas as pd
+
 import pandas_ml_quant_data_provider as qd
-from scraper.dolting import start_server_and_get_engine, mysql_replace_into, dolt_commit, import_df
+from pandas_ml_common import serialize
+from scraper.dolting import start_server_and_get_engine, dolt_commit
+from concurrent.futures import ThreadPoolExecutor
 
 _log = logging.getLogger(__name__)
 
@@ -17,24 +21,27 @@ def download_quotes():
             df.index = pd.MultiIndex.from_product([[symbol], df.index], names=["symbol", "eod"])
             df.columns = [col.lower().replace(" ", "_").replace("__", "_") for col in df.columns]
 
-            return df
+            return symbol, df
         except Exception as e:
             _log.warning(f'failed quote download for {symbol}: {e}')
-            return None
+            return symbol, None
 
     engine = start_server_and_get_engine()
     #symbols = {row[0]: row[1] for row in engine.execute("SELECT o.symbol, coalesce(max(y.eod), '1900-01-06') - INTERVAL 5 DAY FROM optionable o LEFT OUTER JOIN yahoo y on y.symbol = o.symbol GROUP BY o.symbol")}
     symbols = {row[0]: row[1] for row in engine.execute("SELECT symbol, cast('1900-01-01' AS DATE) FROM optionable ORDER BY symbol")}
 
-    for symbol, date in symbols.items():
-        df = fetch(symbol, date)
-        if df is not None:
-            _log.info(f"update db {len(df)} x {df.columns}")
+    # clean up file
+    if os.path.exists('/tmp/quotes.csv'):
+        os.remove('/tmp/quotes.csv')
+
+    with ThreadPoolExecutor(3) as executor:
+        for running_task in executor.map(fetch, symbols.keys(), symbols.values()):
             try:
-                df.to_sql("yahoo", engine, if_exists='append', method=mysql_replace_into)
+                symbol, df = running_task
+                if df is not None:
+                    df.to_hdf('/tmp/quotes.hdf', symbol)
             except Exception as e:
-                _log.error(f"failed to insert data of symbol: {symbol} > {date}: {e}")
-                engine = start_server_and_get_engine()
+                _log.error(f"something failed", e)
 
 
 if __name__ == '__main__':
