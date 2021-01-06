e3614c18d63b6b098368ad6c1d31b2a7e2a278d5
kic <kic@kic.kic>
2021-01-06 18:42:17 +0100

    try using tox test

--- pandas-ml-utils/tox_clean_egg.py
+++ pandas-ml-utils/tox_clean_egg.py
@@ -3,11 +3,15 @@ import shutil
 from pathlib import Path
 
 if __name__ == '__main__':
+    egg_dirs = []
     eggs_home_dir = os.path.dirname(os.path.abspath(__file__))
-    egg_dirs = [os.path.join(eggs_home_dir, dir) for dir in os.listdir(eggs_home_dir) if dir.endswith(".egg-info")]
+    if os.path.exists(eggs_home_dir):
+        egg_dirs = [os.path.join(eggs_home_dir, dir) for dir in os.listdir(eggs_home_dir) if dir.endswith(".egg-info")]
 
+    kernel_dirs = []
     kernels_home_dir = os.path.join(Path.home(), ".local", "share", "jupyter", "kernels")
-    kernel_dirs = [os.path.join(kernels_home_dir, dir) for dir in os.listdir(kernels_home_dir) if "tox" in dir]
+    if os.path.exists(kernels_home_dir):
+        kernel_dirs = [os.path.join(kernels_home_dir, dir) for dir in os.listdir(kernels_home_dir) if "tox" in dir]
 
     for dir in [*egg_dirs, *kernel_dirs]:
         print(f"clean up dir {dir}")
