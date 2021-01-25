c06f3b886f5628a05e30b3574f9479cfaa1379fa
kic <kic@kic.kic>
2021-01-25 19:51:55 +0000

    fix nox test

--- .act/workaround_418.sh
+++ .act/workaround_418.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# see issue https://github.com/nektos/act/issues/418
-# docker build . -t ubuntu-builder
+# install act:
+#   curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+# then
+#   create docker image and alias
+#   see issue https://github.com/nektos/act/issues/418
+#   docker build . -t ubuntu-builder
 alias act="act -P ubuntu-latest=ubuntu-builder"
