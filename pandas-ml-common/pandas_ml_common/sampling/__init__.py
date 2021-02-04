b7d9633b6b3c644a90ea48e3f935546b4e037390
kic <kic@kic.kic>
2021-02-04 15:52:52 +0000

    fix type hint

--- pandas-ml-common/pandas_ml_common/sampling/__init__.py
+++ pandas-ml-common/pandas_ml_common/sampling/__init__.py
@@ -1,2 +1,3 @@
 from .sampler import Sampler, XYWeight
 from .splitter import random_splitter, naive_splitter, dummy_splitter, stratified_random_splitter
+from .cross_validation import KFold, KEquallyWeightEvents, KFoldBoostRareEvents
