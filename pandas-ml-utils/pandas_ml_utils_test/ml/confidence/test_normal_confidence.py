8cd1c81e767c598f0f1407f404bd32894da14d89
kic <kic@kic.kic>
2021-02-10 11:56:05 +0000

    add data to test

--- pandas-ml-utils/pandas_ml_utils_test/ml/confidence/test_normal_confidence.py
+++ pandas-ml-utils/pandas_ml_utils_test/ml/confidence/test_normal_confidence.py
@@ -12,7 +12,7 @@ class TestNormalConfidence(TestCase):
     def test_df_confidence(self):
         std = 0.3
         conf = 0.75
-        df = pd.DataFrame({"a": np.random.normal(0, std, 500)})
+        df = pd.DataFrame({"a": np.random.normal(0, std, 1500)})
         x = pd.Series(np.zeros(len(df)), index=df.index).to_frame().apply(partial(NormalConfidence(conf), std=std), result_type='expand', axis=1)
         tail_events = ((df["a"] >= x[0]) & (df["a"] <= x[1])).values.sum()
         tail_events = tail_events / len(df)
