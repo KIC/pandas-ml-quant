a6ec3246a7e6f18e8332a2d2eacd7e7b2d0d769d
kic <kic@kic.kic>
2021-02-10 12:18:31 +0000

    bump version

--- pandas-ml-common/setup.py
+++ pandas-ml-common/setup.py
@@ -1,5 +1,5 @@
 """Augment pandas DataFrame with methods for machine learning"""
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 import os
 import re
--- pandas-ml-quant-rl/setup.py
+++ pandas-ml-quant-rl/setup.py
@@ -1,5 +1,5 @@
 """Augment pandas DataFrame with methods to fetch time series data for quant finance"""
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 import os
 
