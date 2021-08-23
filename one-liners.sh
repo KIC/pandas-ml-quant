465dc4ad18a60bf405165aa23604445cabb75c03
kic <kic@kic.kic>
2021-08-23 07:50:57 +0000

    fix versions

--- one-liners.sh
+++ one-liners.sh
@@ -2,4 +2,4 @@ echo "script is not meant to run"
 exit 0
 
 # freeze version of a dedicated package
-find -name requirements.txt -exec sed -i -e 's/numpy/numpy==1.20.*/g' {} \;
+find -name requirements.txt -exec sed -i -e 's/numpy.*/numpy==1.20.*/g' {} \;
